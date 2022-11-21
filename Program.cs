93e5f416c2f8c6e80e115a11550232a31e811ba6
Brayan Prates <107517373+Brayanpj@users.noreply.github.com>
2022-11-20 21:14:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Graph;
+using ProjectByteBank.Infra.Context;
+using System.Collections.Generic;
+using System;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(opts => opts.UseSQL(builder.Configuration.GetConnectionString("ListaDeUsuáriosETransaçõesConnection")));
+builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+app.UseMvc();
+app.UseStaticFiles();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ListaDeUsuáriosETransações", Version = "v1" });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetRequiredService<DataContext>();
+    context.Database.EnsureCreated();
+}
+
+app.Run();
+
